fbf0039aaf37314df491c12d347107eccaae81c0
shailesh <shailesh.singh@windmill.digital>
2024-09-09 15:03:57 +0530

    added cicd.yml file

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import uvicorn
+from langchain_openai import ChatOpenAI
+import os
+
+app = FastAPI()
+
+# Initialize OpenAI with your API key
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = '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'
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the FastAPI Langchain Microservice"}
+
+@app.post("/generate-text/")
+async def generate_text(prompt: str):
+    chat = ChatOpenAI(model="gpt-4o", max_tokens = 150)
+    msg = chat.invoke(prompt)
+    return f"generated_text: {msg.content}"
+   
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- test_app.py
+++ test_app.py
@@ -1,1 +1,19 @@
-# python text code
+from fastapi.testclient import TestClient
+from app import app
+
+# Create a TestClient using the FastAPI app
+client = TestClient(app)
+
+def test_root():
+    # Test the root endpoint
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the FastAPI Langchain Microservice"}
+
+def test_generate_text():
+    # Test the text generation endpoint
+    prompt = "Once upon a time"
+    response = client.post("/generate-text/", json={"prompt": prompt})
+    assert response.status_code == 200
+    assert "generated_text" in response.json()
+    assert isinstance(response.json()["generated_text"], str)
