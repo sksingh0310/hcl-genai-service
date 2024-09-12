97918dc94f60b83d124cf5d295197952382c4179
shailesh <shailesh.singh@windmill.digital>
2024-09-12 15:15:26 +0530

    added test_generate_text()

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import uvicorn
 from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class PromptRequest(BaseModel):
+  prompt: str
 # Initialize OpenAI with your API key
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
@@ -16,7 +19,7 @@ async def root():
     return {"message": "Welcome to the FastAPI Langchain Microservice"}
 
 @app.post("/generate-text/")
-async def generate_text(prompt: str):
+async def generate_text(request: PromptRequest):
     """
     Endpoint to generate text based on a given prompt using OpenAI's language model.
 
@@ -27,7 +30,7 @@ async def generate_text(prompt: str):
         str: The generated text from the language model.
     """
     chat = ChatOpenAI(model="gpt-4o-mini", max_tokens = 150)
-    msg = chat.invoke(prompt)
+    msg = chat.invoke(request.prompt)
     return f"generated_text: {msg.content}"
    
 if __name__ == "__main__":
--- test/test_app.py
+++ test/test_app.py
@@ -12,12 +12,14 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the FastAPI Langchain Microservice"}
 
-# def test_generate_text():
-#     # Test the text generation endpoint
-#     prompt = "What is openAI?"
-#     response = client.post("/generate-text/", json={"prompt": prompt})
-#     assert response.status_code == 200
-#     assert "generated_text" in response.json()
-#     assert isinstance(response.json()["generated_text"], str)
+def test_generate_text():
+    # Test the text generation endpoint
+    prompt = "What is openAI?"
+    response = client.post("/generate-text/", json={"prompt": prompt})
+    assert response.status_code == 200
+    assert "generated_text" in response.json()
+    assert isinstance(response.json()["generated_text"], str)
+
+  
 
 
